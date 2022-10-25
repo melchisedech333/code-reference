1bf0e0a92c780c70daa14909c5f0c44e0e1784b4
melchisedech333 <melchisedech333@gmail.com>
2022-10-25 19:14:55 -0300

    Updates

--- rust/tests/variables/target/debug/deps/variables-9e128b8aa9ba134a.d
+++
@@ -1,5 +0,0 @@
-/home/user/projects/code-reference/rust/tests/variables/target/debug/deps/variables-9e128b8aa9ba134a: src/main.rs
-
-/home/user/projects/code-reference/rust/tests/variables/target/debug/deps/variables-9e128b8aa9ba134a.d: src/main.rs
-
-src/main.rs:
--- rust/tests/variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/home/user/projects/code-reference/rust/tests/variables/target/debug/variables: /home/user/projects/code-reference/rust/tests/variables/src/main.rs
