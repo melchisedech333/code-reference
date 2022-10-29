9fdc63c290ee7ba3eeb4f32362eb6bf1272722bd
melchisedech333 <melchisedech333@gmail.com>
2022-10-28 23:06:46 -0300

    Updates

--- erlang/tests/fact.erl
+++ erlang/tests/fact.erl
@@ -1,10 +1,18 @@
+
+% Define um nome para o módulo.
 -module(fact).
+
+% Especifica que a função 'fc' pode ser utilizada por outros módulos.
 -export([fc/1]).
 
-fc(1) ->
-    1;
+%% Define uma função, especificando que para o argumento 1, o 
+%% retorno será sempre 1.
+fc(1) -> 
+    1; % O ponto e virgula significa que a função possui mais código
+       % para ser adicionado a ela.
 
+%% Continua código da função, especificando o que ocorre para outros valores.
 fc(X) ->
-    X * fc(X - 1).
+    X * fc(X - 1). % Conclui função com o ponto.
 
 
